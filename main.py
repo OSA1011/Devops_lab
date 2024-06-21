246f6dfc094f505f46fee80f5270b56880d1dd0d
OSA1011 <Arnautov.semen@gmail.com>
2024-06-21 07:01:20 +0300

    first

---
+++ main.py
@@ -0,0 +1,27 @@
+def sum_of_digits(number: int):
+    print(number)
+    if not isinstance(number, int):
+        raise ValueError
+    number = abs(number)
+    result = 0
+    while number > 0:
+        result += number % 10
+        number //= 10
+    return result
+
+
+def is_lucky(number: int):
+    if not isinstance(number, int):
+        raise ValueError
+    number = abs(number)
+    if number < 10:
+        return True
+    head = sum_of_digits(number // 10 ** ((len(str(number)) + 1) // 2))
+    tail = sum_of_digits(number % 10 ** (len(str(number)) // 2))
+    if head == tail:
+        return True
+    return False
+
+
+print("ok")
+print(sum_of_digits(123))
---
+++ testing.py
@@ -0,0 +1,54 @@
+import pytest
+
+from main import sum_of_digits
+from main import is_lucky
+
+
+@pytest.mark.parametrize(
+    "number, result",
+    [
+        (1234, 10),
+        (-1234, 10),
+        (100, 1),
+        (0, 0),
+        (7, 7)
+    ],
+)
+def test_sum_success(number, result):
+    assert sum_of_digits(number) == result
+
+
+@pytest.mark.parametrize(
+    "number, result",
+    [
+        (15, 5),
+        (10, 10),
+        (-17, 12)
+    ],
+)
+def test_sum_fail(number, result):
+    assert sum_of_digits(number) != result
+
+
+def test_sum_exception():
+    with pytest.raises(ValueError):
+        sum_of_digits('1d2')
+
+
+@pytest.mark.parametrize(
+    "number, result",
+    [
+        (1212, True),
+        (-8035, True),
+        (353, True),
+        (7, True),
+        (135, False)
+    ],
+)
+def test_lucky_success(number, result):
+    assert is_lucky(number) == result
+
+
+def test_lucky_exception():
+    with pytest.raises(ValueError):
+        is_lucky('1d2')
